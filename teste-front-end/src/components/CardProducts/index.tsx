cabf99a1120602583491af2a5cbdfbba54f90746
Rafael540 <alves540@hotmail.com>
2025-08-18 16:53:36 -0300

    Modal implementado

--- teste-front-end/src/components/CardProducts/index.tsx
+++ teste-front-end/src/components/CardProducts/index.tsx
@@ -2,21 +2,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { useEffect, useState, useRef } from "react";
-import left from "../../assets/chevron left.svg"
-import right from "../../assets/chevron right.svg"
+import left from "../../assets/chevron left.svg";
+import right from "../../assets/chevron right.svg";
 
-
-import './styles.scss'
-import { TestModal } from "./TestModal";
-
-
+import "./styles.scss";
+import { TestModal } from "../Modal/TestModal";
 
 
 export default function CardProducts() {
-
-
     const [open, setOpen] = useState<boolean>(false);
-    const [data, setData] = useState([]);
+    const [data, setData] = useState<any[]>([]);
+    const [selectedProduct, setSelectedProduct] = useState<any | null>(null);
+
     const carousel = useRef<HTMLDivElement>(null);
     const [isInitialized, setIsInitialized] = useState(false);
 
@@ -36,99 +33,85 @@ export default function CardProducts() {
         }
     }, [isInitialized]);
 
-
     const handleLeftClick = () => {
         if (carousel.current) {
             carousel.current.scrollBy({ left: -300, behavior: "smooth" });
         }
-    }
+    };
 
     const handleRightClick = () => {
         if (carousel.current) {
             carousel.current.scrollBy({ left: 300, behavior: "smooth" });
         }
-    }
+    };
 
     const handleProductClick = (product: any) => {
-        console.log("Abrindo modal", product);
-        //setSelectedProduct(product);
+        setSelectedProduct(product);
         setOpen(true);
-    }
-
+    };
 
     if (!data || !data.length) return null;
 
     return (
-        <><section className="ec-container">
+        <>
+            <section className="ec-container">
+                <div
+                    ref={carousel}
+                    className="ec-carousel"
+                    style={{
+                        display: "flex",
+                        overflowX: "hidden",
+                        gap: "18px",
+                        justifyContent: "space-between",
+                        scrollBehavior: "smooth",
+                        paddingLeft: "16px",
+                        height: "503px",
+                    }}
+                >
+                    <div className="ec-buttons ec-pdlf">
+                        <button onClick={handleLeftClick} className="arrow-right">
+                            <img src={left} alt="Scroll Left" />
+                        </button>
+                    </div>
 
-            <div ref={carousel}
-                className="ec-carousel"
-                style={{
-                    display: "flex",
-                    overflowX: "hidden",
-                    gap: "18px",
-                    justifyContent: "space-between",
-                    scrollBehavior: "smooth",
-                    paddingLeft: "16px",
-                    height: '503px',
-                    backgroundColor: 'none',
+                    {data.map((item) => {
+                        const { productName, photo, price, descriptionShort } = item;
+                        return (
+                            <div className="ec-product" key={productName}>
+                                <div className="ec-product-box">
+                                    <img className="ec-product-img" src={photo} alt={productName} />
 
+                                    <div className="ec-product-el">
+                                        <p className="ec-product-dsc">{descriptionShort}</p>
+                                        <p className="ec-product-lp">R$ 30,90</p>
+                                        <h2 className="ec-product-p">R$ {price}</h2>
+                                        <p className="ec-product-fees">ou 2x de R$ 49,95 sem juros</p>
+                                        <p className="ec-product-dl">Frete grátis</p>
 
-
-                }}>
-                <div className='ec-buttons ec-pdlf '>
-                    <button onClick={handleLeftClick} className="arrow-right">
-                        <img src={left} alt="Scroll Right" /></button>
-                </div>
-
-                {data.map((item) => {
-                    const { productName, photo, price, descriptionShort } = item;
-                    return (
-
-                        <div className='ec-product' key={productName} >
-
-
-                            <div className="ec-product-box" >
-                                <img className="ec-product-img" src={photo} alt={productName} />
-
-
-                                <div className="ec-product-el">
-                                    <p className="ec-product-dsc">{descriptionShort}</p>
-                                    <p className="ec-product-lp" >R$ 30,90</p>
-                                    <h2 className="ec-product-p">R$ {price}</h2>
-                                    <p className="ec-product-fees">ou 2x de R$ 49,95 sem juros</p>
-                                    <p className="ec-product-dl"> Frete grátis</p>
-
-                                    <div className='ec-button-primary'>
-                                        <button onClick={() => setOpen(!open)}> Comprar </button>
-
+                                        <div className="ec-button-primary">
+                                            <button onClick={() => handleProductClick(item)}>
+                                                Comprar
+                                            </button>
+                                        </div>
                                     </div>
-
-
                                 </div>
                             </div>
+                        );
+                    })}
 
-
-
-                        </div>
-
-
-                    );
-
-                })}
-                <div className='ec-buttons ec-pdrg'>
-                    <button onClick={handleRightClick} className="arrow-right"><img src={right} alt="Scroll Right" /></button>
+                    <div className="ec-buttons ec-pdrg">
+                        <button onClick={handleRightClick} className="arrow-right">
+                            <img src={right} alt="Scroll Right" />
+                        </button>
+                    </div>
                 </div>
-                <TestModal isOpen={open} setOpen={setOpen} />
-            </div>
+            </section>
 
-
-
-        </section>
-
-
-
+            {/* Modal */}
+            {selectedProduct && (
+                <TestModal isOpen={open} setOpen={setOpen} product={selectedProduct}
+                />
+            )}
         </>
-
     );
 }
---
+++ teste-front-end/src/components/Modal/TestModal.tsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import type { Product } from "../../types/product";
+import x from '../../assets/X.svg'
+
+
+
+interface ModalProps {
+    isOpen: boolean;
+    setOpen: (isOpen: boolean) => void;
+    product: Product | null;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const TestModal = ({ isOpen, setOpen, product }: ModalProps) => {
+
+    if (!isOpen) return null;
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [count, setCount] = useState(0);
+
+    function handleIncrease() {
+
+        const soma = count + 1;
+        setCount(soma)
+
+
+    }
+    function handleDecrease() {
+        if (count != 0) {
+            const soma = count - 1;
+            setCount(soma)
+        }
+
+
+
+    }
+
+    return (
+
+        <div style={{
+            position: 'fixed', width: '100%', height: '100%', top: '0', left: '0',
+            background: 'rgba(0,0,0,0.6)', zIndex: 9999, display: 'flex', justifyContent: 'center',
+            alignItems: 'center', color: 'white', fontSize: '2rem'
+        }} // fecha ao clicar fora
+        >
+            {/*Card*/}
+            <div className="modal-content" style={{
+                width: "769px", height: '300px', backgroundColor: 'white', color: 'black', padding: '16px',
+                borderRadius: '8px', display: 'flex', gap: '20px'
+            }} onClick={e => e.stopPropagation()}>
+
+                {/*Imagem*/}
+                <div style={{ flex: "0 0 auto", float: "right", marginTop: "40px", marginLeft:'40px' }}>
+                    <img src={product?.photo} alt={product?.productName} style={{ maxWidth: "247px", height: "192.199px", borderRadius: "8px" }}
+                    />
+                </div>
+
+               
+
+                {/*Div descrição*/}
+                <div style={{ 
+                    flex: "1", float: "left", width: "328px", height: "198px", marginLeft: "87px",
+                    marginTop: "40px"
+                }}>
+                    {/*Descrição*/}
+                    <div>
+                        <p style={{
+                            height: "16.86px", alignSelf: "stretch",
+                            fontSize: "20px", width: "300px", textTransform: "uppercase", marginBottom: '16px'
+                        }}> {product?.descriptionShort}</p>
+                        {/*Preço*/}
+                        <p style={{ height: "17px", width: "328px", fontSize: "20px", fontWeight: 600, marginBottom: "32px", color: '#3F3F40' }}>R$ {product?.price} </p>
+
+                    </div>
+
+                    {/*Detalhes*/}
+                    <div style={{ width: '300px', marginBottom: '32px' }}>
+                        <p style={{ fontSize: '10px', alignSelf: "strech", width: "290px", height: '30px', marginBottom: "18px", color: '#9F9F9F' }}>  Many desktop publishing packages and web page editors now many desktop publishing </p>
+
+                        <p style={{ height: '10px', fontSize: '11px', color: "#3442B5", fontWeight: 600 }}>Veja mais detalhes do produto </p>
+                    </div>
+
+                    {/*Botões*/}
+                    <div style={{ width: '310px', height: '36px', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', marginBottom: '8px' }}>
+
+                        {/*Quantidade*/}
+                        <div style={{
+                            width: "110px", height: "36px", display: 'flex', alignItems: "center", border: '1px solid #D9D9D9',
+                            justifyContent: "space-between", borderRadius: '4px', boxSizing: 'border-box', paddingRight: '12px'
+                        }}>
+                            <button style={{ backgroundColor: "#fff", width: "20.308px", height: "19.636px", marginLeft: '12px', border: 'none' }} onClick={handleDecrease}> -</button>
+                            <p style={{ fontSize: '16px', }}>{count}</p>
+                            <button style={{ backgroundColor: "#fff", width: "20.308px", height: "19.636px", border: 'none' }} onClick={handleIncrease} > +</button>
+                        </div>
+
+                        {/*Botão comprar*/}
+                        <div style={{ alignItems: "center", marginLeft: '12px', marginBottom: '8px', }}>
+                            <button style={{
+                                width: '188px', height: "36px", justifyContent: 'center', textTransform: 'uppercase',
+                                alignItems: 'center', borderRadius: '6px', border: "none", gap: '10px', backgroundColor: '#F7CA11',
+                                fontSize: '14px', fontWeight: '700', textAlign: 'center', marginBottom: '4px'
+
+                            }}>Comprar</button>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+                 <div style={{height:'260px',width:'22px', marginRight:'44px', justifyContent:'top'}}>
+                    <button onClick={() => setOpen(false)}  style={{border:'none', backgroundColor:'#fff'}}><img src={x} alt="Exit"  style={{marginBottom:'24px',width:'15px', height:'13px'}}/></button>
+                </div>
+            </div>
+        </div>
+    );
+};
